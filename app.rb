a8f037e3148ab98c5b23b6b2f4be7df1f76bfe6b
Johnson Joseph <jhonsonj88@gmail.com>
2019-04-24 03:38:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+
+    #@team = ['Louis Armstrong', 'Ella Fitzgerald', 'Mile Davis', 'Dave Brubeck', 'John Coltrane', 'Sonny Rollins']
+
+    erb :team
+  end
+
 
 end
